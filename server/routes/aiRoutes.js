64f1a8c5fd6463e2fab7abdcdd7d7105008305c4
Soham Mule <sohammule09@gmail.com>
2026-03-08 08:15:05 +0000

    added langchain and langraph

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { auth } from "../middleware/auth.js"
-import { GenerateArticle, GenerateBlogTitle, GenerateImage, RemoveImageBackground, RemoveImageObject, ResumeReview } from "../controllers/aiController.js"
+import { askaboutResume, GenerateArticle, GenerateBlogTitle, GenerateImage, RemoveImageBackground, RemoveImageObject, ResumeReview, ReviewResume } from "../controllers/aiController.js"
 import { upload } from "../configs/multer.js"
 
 const aiRouter = express.Router()
@@ -12,5 +12,7 @@ aiRouter.post("/generate-image", auth, GenerateImage)
 aiRouter.post("/remove-image-background", auth, upload.single('image'), RemoveImageBackground)
 aiRouter.post("/remove-image-object", auth, upload.single('image'), RemoveImageObject)
 aiRouter.post("/resume-review", auth, upload.single('resume'), ResumeReview)
+aiRouter.post("/resume", auth, upload.single('resume'), ReviewResume)
+aiRouter.post("/askabout", auth, askaboutResume)
 
 export default aiRouter
