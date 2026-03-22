4866f3777d844b9e1ded4522be9982f2ce5af389
Soham Mule <sohammule09@gmail.com>
2026-03-22 11:49:38 +0000

    new-changes

--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -2,7 +2,7 @@ import { describe, expect, it } from "@jest/globals"
 import request from "supertest"
 
 describe("Server Health Check", () => {
-    it("Should return 200 on health endpoing", async () => {
+    it("Should return 200 on health endpoint", async () => {
         expect(true).toBe(true)
     })
 })
